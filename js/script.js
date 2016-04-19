968cf47bcaf6c03ecf7f6756d09ffcc424d633b9
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-04-19 17:58:46 +0200

    :art: Home

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 /**** VARIABLES ****/
+var myScroll;
+
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(callback){ window.setTimeout(callback, 1000/60); };
+})();
+
+// Request anim frame
+function scrollPage(){
+	myScroll = $(document).scrollTop();
+	
+	requestAnimFrame(scrollPage);
+}
 
 // Alternative au CSS object-fit
 // Fonction pour adapter la taille d'une image à son container
@@ -19,8 +36,32 @@ function imgFit(){
 
 /**** INIT ****/
 $(function(){
+	scrollPage();
 	imgFit();
+	TweenMax.set($('.social-footer'), {className:'+=closed'});
+	$('.social-footer').hover(
+		function(){
+			if($(this).hasClass('closed')){
+				TweenMax.set($(this), {className:'-=closed'});
+			}
+		}, function(){
+			var scrollTopV = $(document).scrollTop();
+			if(!(myScroll + $(window).height() == $(document).height())) {
+				TweenMax.set($(this), {className:'+=closed'});
+			}
+		}
+	);
     $(document).scroll(function(){
+    	myScroll = $(document).scrollTop();
+    	if(myScroll + $(window).height() == $(document).height()) {
+    		if($('.social-footer').hasClass('closed')){
+    			TweenMax.set($('.social-footer'), {className:'-=closed'});
+    		}
+    	}else{
+    		if(!$('.social-footer').hasClass('closed')){
+    			TweenMax.set($('.social-footer'), {className:'+=closed'});
+    		}
+    	}
     });
 
     $(window).resize(function(){
