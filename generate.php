1e15a57e4e7627e4dd3407ee46276ef8321173c9
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-05-11 14:17:41 +0200

    :art: generate html files
    To generate html files:
    - add your new file in generate.php
    - launch the script by accessing to http://localhost/generate.php
    - if a file already exists it'll be override

---
+++ generate.php
@@ -0,0 +1,14 @@
+<?php
+    copy('http://localhost/index.php', 'index.html');
+    copy('http://localhost/actu-detail.php', 'actu-detail.html');
+    copy('http://localhost/mentions.php', 'mentions.html');
+
+    copy('http://localhost/monCompte.php', 'monCompte.html');
+    copy('http://localhost/monCompte_error.php', 'monCompte_error.html');
+    copy('http://localhost/monCompteAlertes.php', 'monCompteAlertes.html');
+    copy('http://localhost/monCompteAlerteEdition.php', 'monCompteAlerteEdition.html');
+    copy('http://localhost/monCompteAlertesEmpty.php', 'monCompteAlertesEmpty.html');
+    copy('http://localhost/monCompteCorrespondance.php', 'monCompteCorrespondance.html');
+    copy('http://localhost/monCompteSelection.php', 'monCompteSelection.html');
+    copy('http://localhost/monCompteVentes.php', 'monCompteVentes.html');
+?>
