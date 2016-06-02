e8974781516ab263d98dfd1893b86c26cc988781
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-06-02 17:55:52 +0200

    :art: html generation

--- includes/header_auth_on.php
+++ includes/header_auth_on.php
@@ -5,39 +5,42 @@
                     <?php echo isset($field) ? $field : 'Forum des Commerces'; ?>
                 </a>
             </div>
-            <nav role='navigation'>
-                <button id="btn-close-nav-header" class="btn-close icon-cross"></button>
-                <ul class='menu-header'>
-                    <li>
-                        <a href='#'>Qui sommes nous ?</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Achat</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Partenaires</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Matériel</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Personnel</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Vente</a>
-                    </li>
-                </ul>
-                <ul class='nav-header'>
-                    <li>
-                        <a href='#'><span class='icon-mail'></span><span class='txt-nav'>Contact</span></a>
-                    </li><!--
-                    --><li>
-                        <button id='btn-tools'><span class='icon-bulb'></span><span class='txt-nav'>Paramètres</span></button>
-                    </li><!--
-                    --><li>
-                        <button id='btn-account'><span class='icon-user'></span><span class='txt-nav'>S'identifier</span></button>
-                    </li>
-                </ul>
+            <nav role='navigation' id='nav-header'>
+                <button id='btn-close-nav-header' class='btn-close icon-cross'></button>
+                <div class='wrapper-content-nav'>
+                    <div class='content-nav-header'>
+                        <ul class='menu-header'>
+                            <li>
+                                <a href='#'>Qui sommes nous ?</a>
+                            </li><!--
+                            --><li>
+                                <a href='#'>Achat</a>
+                            </li><!--
+                            --><li>
+                                <a href='#'>Partenaires</a>
+                            </li><!--
+                            --><li>
+                                <a href='#'>Matériel</a>
+                            </li><!--
+                            --><li>
+                                <a href='#'>Personnel</a>
+                            </li><!--
+                            --><li>
+                                <a href='#'>Vente</a>
+                            </li>
+                        </ul><ul class='nav-header'>
+                            <li>
+                                <a href='#'><span class='icon-mail'></span><span class='txt-nav'>Contact</span></a>
+                            </li><!--
+                            --><li>
+                                <button id='btn-tools'><span class='icon-bulb'></span><span class='txt-nav'>Paramètres</span></button>
+                            </li><!--
+                            --><li>
+                                <button id='btn-account'><span class='icon-user'></span><span class='txt-nav'>S'identifier</span></button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </header>
 
@@ -50,65 +53,68 @@
         </a>
 
         <button id='btn-close-header-responsive' class='btn-close icon-cross'></button>
-
+        
+        <div class='bg-sidebar'></div>
         <aside id='sidebar-links' class='sidebar'>
             <button id='btn-close-sidebar-links' class='btn-close icon-cross'></button>
-            <p class='small'>16 sites d'annonces dédiés aux entrepreneurs et spécialisés dans leurs activités respectives&nbsp;:</p>
-            <nav>
-                <ul class='menu-sidebar'>
-                    <li>
-                        <a href='#'>Bar Tabac PMU</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Bar Brasserie</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Boucherie Charcuterie Traiteur</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Boulangerie Pâtisserie</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Boutique et Magasins</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Bureaux Locaux Entrepôts</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Coiffure Esthétique Parfumerie</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Commerce Alimentation</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Creperie Pizzeria</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Entreprise du bâtiment</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Hôtel Bureau</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Hôtel Restaurant</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Murs Commerciaux</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Restaurant</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Restauration rapide</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Salon de thé</a>
-                    </li><!--
-                    --><li>
-                        <a href='#'>Tabac Loto Presse</a>
-                    </li>
-                </ul>
-            </nav>
+            <div class='wrapper-content-sidebar'>
+                <p class='small'>16 sites d'annonces dédiés aux entrepreneurs et spécialisés dans leurs activités respectives&nbsp;:</p>
+                <nav>
+                    <ul class='menu-sidebar'>
+                        <li>
+                            <a href='#'>Bar Tabac PMU</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Bar Brasserie</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Boucherie Charcuterie Traiteur</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Boulangerie Pâtisserie</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Boutique et Magasins</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Bureaux Locaux Entrepôts</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Coiffure Esthétique Parfumerie</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Commerce Alimentation</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Creperie Pizzeria</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Entreprise du bâtiment</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Hôtel Bureau</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Hôtel Restaurant</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Murs Commerciaux</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Restaurant</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Restauration rapide</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Salon de thé</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Tabac Loto Presse</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </aside>
 
         <aside id='sidebar-account' class='sidebar'>
@@ -144,24 +150,26 @@
 
         <aside id='sidebar-tools' class='sidebar'>
             <button id='btn-close-sidebar-tools' class='btn-close icon-cross'></button>
-            <div class='title-sidebar'>Outils</div>
-            <nav>
-                <ul class='menu-sidebar'>
-                    <li>
-                        <a href="#">FAQ</a>
-                    </li><!--
-                    --><li>
-                        <a href="#">Liens utiles</a>
-                    </li><!--
-                    --><li>
-                        <a href="#">ABC du métier</a>
-                    </li><!--
-                    --><li>
-                        <a href="#">Glossaire</a>
-                    </li><!--
-                    --><li>
-                        <a href="#">Offres et Tarifs</a>
-                    </li>
-                </ul>
-            </nav>
+            <div class='wrapper-content-sidebar'>
+                <div class='title-sidebar'>Outils</div>
+                <nav>
+                    <ul class='menu-sidebar'>
+                        <li>
+                            <a href="#">FAQ</a>
+                        </li><!--
+                        --><li>
+                            <a href="#">Liens utiles</a>
+                        </li><!--
+                        --><li>
+                            <a href="#">ABC du métier</a>
+                        </li><!--
+                        --><li>
+                            <a href="#">Glossaire</a>
+                        </li><!--
+                        --><li>
+                            <a href="#">Offres et Tarifs</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </aside>
