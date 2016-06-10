11dec9dbbd45852a191a63bdb9956f7c35c4a896
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-06-10 12:11:16 +0200

    :art: Activities | search

--- activite_bartabac.php
+++ activite_bartabac.php
@@ -47,7 +47,7 @@
 									<button class='btn-close-choice icon-cross'></button>
 									<div class='field-content searchform'>
 				                        <div class='tab-content-title'>
-				                            <h2 class='h1'>Recherche</h2>
+				                            Recherche
 				                        </div>
 				                        <form action='#' method='post' class='form-half'>
 				                            <fieldset>
@@ -484,35 +484,39 @@
 					</div>
 				</div>
 
-				<h2 class='h1 inline-b no-margin padding-grid'>À la une</h2>
+				<div class='title-zone inline-b no-margin padding-grid'>À la une</div>
 				<a href='#' class='btn-arrow small inline-b'>Toute l'actualité</a>
 				<ul class='news'>
 					<li>
 						<article>
-							<div class='center-align article-date'>
-								<div class='wrapper-alignment full-height'>
-									<time class='content-align-middle' datetime='2016-03-25'><span class='day'>25</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
-								</div>
-							</div><!--
-							--><div class='article-desc'>
-								<h4 class='small'>Actualité fiscale</h4>
-								<h3 class='h2'>Crédit d’impôt : tout un art…</h3>
-								<p>Une entreprise exerçant une activité de conseil en communication et en création publicitaire a sollicité le bénéfice… <a href='#' class='btn-arrow'>Lire la suite</a></p>
-							</div>
+							<a href='#'>
+								<span class='center-align article-date'>
+									<span class='wrapper-alignment full-height'>
+										<time class='content-align-middle' datetime='2016-03-25'><span class='day'>25</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+									</span>
+								</span><!--
+								--><span class='article-desc'>
+									<h4 class='small'>Actualité fiscale</h4>
+									<h5>Crédit d’impôt : tout un art…</h5>
+									<p>Une entreprise exerçant une activité de conseil en communication et en création publicitaire a sollicité le bénéfice… <span class='btn-arrow'>Lire la suite</span></p>
+								</span>
+							</a>
 						</article>
 					</li><!--
 					--><li>
 						<article>
-							<div class='center-align article-date'>
-								<div class='wrapper-alignment full-height'>
-									<time class='content-align-middle' datetime='2016-03-25'><span class='day'>25</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
-								</div>
-							</div><!--
-							--><div class='article-desc'>
-								<h4 class='small'>Actualité sociale</h4>
-								<h3 class='h2'>Des indemnités de préavis en cas de maladie ?</h3>
-								<p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <a href='#' class='btn-arrow'>Lire la suite</a></p>
-							</div>
+							<a href='#'>
+								<span class='center-align article-date'>
+									<span class='wrapper-alignment full-height'>
+										<time class='content-align-middle' datetime='2016-03-25'><span class='day'>25</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+									</span>
+								</span><!--
+								--><span class='article-desc'>
+									<h4 class='small'>Actualité sociale</h4>
+									<h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+									<p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+								</span>
+							</a>
 						</article>
 					</li>
 				</ul>
