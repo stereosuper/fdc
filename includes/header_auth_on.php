3e5ec8ba80d31ad97e10809b20b8aa8e10af6581
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-07-06 15:16:51 +0200

    :bug: Logo background color Safari

--- includes/header_auth_on.php
+++ includes/header_auth_on.php
@@ -49,7 +49,6 @@
         <a href='#' id='logo'>
             <span class='rect-logo'></span><span class='rect-logo'></span><span class='rect-logo'></span>
             <span id='burger'><span></span><span></span><span></span></span>
-            <img src='img/logo.png' srcset='img/logo.png 1x, img/logo@2x.png 2x' alt='Forum des Commerces'>
         </a>
 
         <button id='btn-close-header-responsive' class='btn-close icon-cross'></button>
