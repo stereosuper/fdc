f13efac8b44263e273f44cbb96bcbc6844f2bec2
Boris Forconi <boris@stereosuper.fr>
2016-07-12 18:54:42 +0200

    page detail
    responsive à venir

---
+++ 126_achat_detail_connecte.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Forum des Commerces - Achat résultats de recherche</title>
+        <?php include_once('includes/head.html'); ?>
+
+        <link rel='prev' href='/page1.html'>
+        <link rel='next' href='/page2.html'>
+    </head>
+
+    <body>
+        <?php
+            global $field;
+            $field = 'Boulangerie - Pâtisserie - Salon de thé';
+            include_once('includes/header.php');
+        ?>
+
+        <main id='main' role='main'>
+            <div class='bloc-top'>
+                <div class='wrapper-img img-fit'>
+                    <img src='img/visuel-home.jpg' alt=''>
+                </div>
+            </div>
+            <div class='container'>
+                <div class="table-layout detail">
+                    <div class="content-tab-layout">
+                        <div class='field field-buy'>
+                            <header class='field-header'>
+                                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                                    <li property='itemListElement' typeof='ListItem'>
+                                        <h6><a property='item' typeof='WebPage' href='./'>
+                                            <span property='name'>Accueil</span>
+                                        </a></h6>
+                                    </li>
+                                    <li property='itemListElement' typeof='ListItem'>
+                                        <h6><a property='item' typeof='WebPage' href='#'>
+                                            <span property='name'>Achat de boulangerie - pâtisserie - salon de thé</span>
+                                        </a></h6>
+                                    </li>
+                                    <li property='itemListElement' typeof='ListItem'>
+                                        <h6><a property='item' typeof='WebPage' href='#'>
+                                            <span property='name'>Résultats</span>
+                                        </a></h6>
+                                    </li>
+                                </ul>
+                                <h1>Boulangerie pâtisserie en Vendée</h1>
+                                <div class='rect'></div>
+                            </header>
+                        </div>
+                        <div class='table table-layout'>
+                            <div class="content-tabs-detail content-tab-layout">
+                                <div id="map-canvas"></div>
+                                <div class='field-content-detail'>
+                                    <mark class="detail-ref">Achat - Ref 1254</mark>
+                                    <p>Boulangerie-Pâtisserie située dans une ville du littoral Atlantique. Ce produit est idéal pour une première installation, il n'y a aucun travaux à prévoir. Proche tous commerces et administratif. Emplacement N&nbsp;°1. Matériel et agencement complets. Logement F3 avec petite cour intérieure. Marge de progression. Produit exceptionnel à voir rapidement</p>
+                                    <ul>
+                                        <li>Département&nbsp;: <strong>Vendée</strong></li>
+                                        <li>Ville&nbsp;: <strong>Saint Jean de Monts</strong></li>
+                                        <li>Type de vente&nbsp;: <strong>fonds</strong></li>
+                                        <li>Masse salariale&nbsp;: <strong>1</strong></li>
+                                        <li>CA HT du dernier exercice&nbsp;: <strong>112&#8239;730&nbsp;€</strong></li>
+                                        <li>EBE&nbsp;: <strong>38&#8239;886&nbsp;€</strong></li>
+                                    </ul>
+                                    <p><em>Annonce mise en ligne le 19/04/2016</em></p>
+                                    <div class='content-footer'>
+                                        <a href="#" class='btn-arrow-back'>Retour à la recherche</a>
+                                        <a href="#" class='btn-print'>Imprimer</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class='content-tabs-detail content-tab-layout'>
+                                <ul class="detail-photos"><!-- POUR NETIL : NE PAS METTRE DE UL SI VIDE -->
+                                    <li><a href="img/comptoireBoul_Big.png" href="#"><img src="img/comptoireBoul.png"></a></li>
+                                </ul>
+                                <div class='field-content-detail'>
+                                    <p class='big-price'>Prix&nbsp;: 50&#8239;000&nbsp;€</p>
+                                    <p><strong>Cabinet d'affaires d'Artagnan</strong></p>
+                                    <p>7 avenue des Cols Verts</p>
+                                    <p>33115 LE PYLA SUR MER</p>
+                                    <p>Téléphone&nbsp;: <strong>05 53 87 20 00</strong></p>
+                                    <a href="#" class='btn-arrow'>Écrire à l'annonceur</a>
+                                    <div class='content-footer'>
+                                        <a href="#" class='btn-heart' >Ajouter à ma séléction</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='wrapper-pub pub-right content-tab-layout'>
+                        <div class='pub'>
+                            <div class='indic-pub'>— publicité —</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
