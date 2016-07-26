0ab4e33d039f4f69072d649d862368989e5c768c
Boris Forconi <boris@stereosuper.fr>
2016-07-26 16:58:18 +0200

    finalement, passage aux images

--- 600_accompagnement_bienvenue.php
+++ 600_accompagnement_bienvenue.php
@@ -41,14 +41,6 @@
                         <header class='tab-content-title'>
                             <h2 class='h1'>Faites-vous accompagner&nbsp;!</h2>
                         </header>
-                        <style>
-                            @media (min-width: 1079px){
-                                .cercleMyForum ol.full-circle li:before {
-                                    clip-path: url("#clipPolygon");
-                                }
-                            }
-                            /* dirty for firefox */
-                        </style>
                         <ol class="full-circle">
                             <li class="buy-step">Acheter</li><!--
                             --><li class="finance-step">Financer</li><!--
@@ -58,13 +50,6 @@
                             --><li class="manage-step">Gérer</li><!--
                             --><li class="sell-step">Vendre</li>
                         </ol>
-                        <svg width="0" height="0">
-                            <defs>
-                                <clipPath id="clipPolygon">
-                                    <polygon points="68 0, 136 68, 68 136, 0 68" />
-                                </clipPath>
-                            </defs>
-                        </svg>
                         <div class="circle-text">
                             <img src="./layoutImg/cercle/cercle.png" width="198" alt="Le Cercle MyForum"/><!--
                             --><p>Pour réussir et vous simplifier la vie, quel que soit votre besoin d’accompagnement, un interlocuteur dédié du Cercle Forum des Commerces vous contacte, vous conseille et vous met en relation vers le bon expert.</p>
