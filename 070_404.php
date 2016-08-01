91c2a4cd90c3beaeaa05222bfe3184a87c403e9c
Boris Forconi <boris@stereosuper.fr>
2016-08-01 17:58:33 +0200

    404

---
+++ 070_404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Mentions légales</title>
+        <?php include_once('includes/head.html'); ?>
+    </head>
+
+    <body>
+        <?php include_once('includes/header.php'); ?>
+
+        <main id='main' role='main'>
+            <div class='container-small'>
+                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <h6><a property='item' typeof='WebPage' href='./'>
+                            <span property='name'>Accueil</span>
+                        </a></h6>
+                    </li>
+                </ul>
+
+                <h1>Erreur 404</h1>
+                <div class='content-default content-right'>
+                    <h3>La page que vous recherchez n’existe pas&nbsp;!</h3>
+                    <img src="./img/giphy.gif" width="400" alt="lol" class="no-float"/>
+                    <p>Nous vous invitons à repartir de <a href="#">la page accueil.</a></p>
+                    <p>Peut-être pouvons nous vous aider. Que recherchiez vous&nbsp;?</p>
+                    <ul><li><a href="#">à vendre</a> une entreprise ou un fond de commerce</li>
+                    <li><a href="#">à acheter</a> une entreprise ou un fond de commerce</li>
+                    <li><a href="#">à vous faire accompagner</a> pour votre entreprise ou votre fond de commerce</li></ul>
+                </div>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
