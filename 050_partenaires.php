e5e869953e14b0f67e45b06f996305d9c4762ce7
Boris Forconi <boris@stereosuper.fr>
2016-08-16 19:26:36 +0200

    fix de la saloperie d’Adrien en 420

---
+++ 050_partenaires.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Mon compte - Infos</title>
+        <?php include_once('includes/head.html'); ?>
+    </head>
+
+    <body>
+        <?php include_once('includes/header.php'); ?>
+
+        <main id='main' role='main'>
+            <div class='container-small'>
+                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='./'>
+                            <span property='name'>Accueil</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <h1>Partenaires</h1>
+                <div class='cercleMyForum adCercle small-margin'>
+                     <div class='field-content'>
+                        <ul class="little-squares wave"><li></li><li></li><li></li><li></li><li></li><li></li></ul>
+                        <header>
+                            <img src="./layoutImg/cercle/cercle.png" width="198" alt="Le Cercle MyForum"/>
+                            <h2 class='h1'>Facilitateur<br /> de tous vos projets</h2>
+                            <p>Pour réussir et vous simplifier la vie, faites-vous (bien) accompagner par un membre du Cercle&nbsp;!</p>
+                        </header>
+                        <ol class="full-circle wave">
+                            <li class="buy-step">Acheter</li><!--
+                            --><li class="finance-step">Financer</li><!--
+                            --><li class="ensure-step">Assurer</li><!--
+                            --><li class="equip-step">Équiper</li><!--
+                            --><li class="staff-step">Recruter</li><!--
+                            --><li class="manage-step">Gérer</li><!--
+                            --><li class="sell-step">Vendre</li>
+                        </ol>
+                        <div class="circle-text"><div>
+                            <p><img src="./layoutImg/cercle/serviceplus.png" width="104" alt="Service Gratuit" class="gratuit" /> Quel que soit votre besoin d'accompagnement, votre interlocuteur dédié du Cercle Forum des Commerces vous contacte, vous conseille et vous met en relation vers le bon expert. </p><a href="#" class='btn-block btn-yellow medium'><span class='border'></span><span class='txt-btn'>Me faire accompagner</span></a>
+                        </div></div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
