8c81ef734bd1d7b0ede0a748a72b0abcdc1a2a5c
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-09-21 16:44:55 +0200

    :art: Actu categories / liste

---
+++ 021_actu_categories.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Actu détail</title>
+        <?php include_once('includes/head.html'); ?>
+    </head>
+
+    <body>
+        <?php include_once('includes/header.php'); ?>
+
+        <main id='main' role='main'>
+            <div class='container-small'>
+                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='./'>
+                            <span property='name'>Accueil</span>
+                        </a>
+                    </li>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='#'>
+                            <span property='name'>Actualités</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <h1>Actualités</h1>
+                <ul class='field-list categs clearfix'>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>Actu fiscale</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio in posuere convallis.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>Actu Juridique</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio in posuere convallis.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>Actu Sociale</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio in posuere convallis.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>La petite histoire</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio in posuere convallis.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>Le coin du dirigeant</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio in posuere convallis.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span class='field-txt'>
+                                <h4 class='title-simple-big'>Rappels d’obligations, chiffres et barèmes utiles</h4>
+                                <span class='p'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas dignissim ullamcorper. Integer hendrerit odio.
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
---
+++ 022_actu_liste.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Actu détail</title>
+        <?php include_once('includes/head.html'); ?>
+    </head>
+
+    <body>
+        <?php include_once('includes/header.php'); ?>
+
+        <main id='main' role='main'>
+            <div class='container-small'>
+                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='./'>
+                            <span property='name'>Accueil</span>
+                        </a>
+                    </li>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='#'>
+                            <span property='name'>Actualités</span>
+                        </a>
+                    </li>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <a property='item' typeof='WebPage' href='#'>
+                            <span property='name'>Actu Fiscale</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <h1>Actu Fiscale</h1>
+                <ul class='news news-list clearfix'>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>25</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>26</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>27</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>28</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>29</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                    <li>
+                        <article>
+                            <a href='#'>
+                                <span class='center-align article-date'>
+                                    <span class='wrapper-alignment full-height'>
+                                        <time class='content-align-middle' datetime='2016-03-25'><span class='day'>30</span> <span class='month'>mars</span> <span class='year'>2016</span></time>
+                                    </span>
+                                </span><!--
+                                --><span class='article-desc'>
+                                    <h4 class='small'>Actualité sociale</h4>
+                                    <h5>Des indemnités de préavis en cas de maladie&nbsp;?</h5>
+                                    <p>Un employeur licencie un salarié pour motif économique. Cependant, ce salarié est en arrêt maladie… <span class='btn-arrow'>Lire la suite</span></p>
+                                </span>
+                            </a>
+                        </article>
+                    </li>
+                </ul>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
