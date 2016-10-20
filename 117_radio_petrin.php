41db244673f47be8f64058693a6051dda6ebefd3
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-10-20 15:55:19 +0200

    :art: bloc vendre -> 2eme position

--- 117_radio_petrin.php
+++ 117_radio_petrin.php
@@ -169,6 +169,151 @@
 								</div>
 							</div>
 						</li><!--
+						--><li>
+							<div class='zone-btn'>
+								<a href='#' class='btn-choice'></a>
+								<div class='bg-choice'></div>
+								<div class='visuel'>
+									<div class='wrapper-alignment'>
+										<div class='content-align-middle'>
+											<img src="img/vente.png" srcset="img/vente.png 1x, img/vente@2x.png 2x" alt="">
+										</div>
+									</div>
+								</div>
+								<div class='zone-txt wrapper-alignment'>
+									<div class='content-align-middle'>
+										<h3>Vendre <br />son affaire</h3>
+										<p>Vente d'un fond de commerce, d'une entreprise, d'un bureau, d'un local&nbsp;commercial…</p>
+									</div>
+								</div>
+							</div><!--
+							--><div class='zone-content'>
+								<div class='content'>
+									<button class='btn-close-choice icon-cross'></button>
+									<table cellpadding='0' cellspacing='0' border='0' class='table-types'>
+										<tr>
+											<td>
+												<a href='#'>Bar Tabac PMU</a>
+											</td>
+											<td>
+												<a href='#'>Coiffure Esthétique Parfumerie</a>
+											</td>
+											<td>
+												<a href='#'>Murs Commerciaux</a>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href='#'>Bar Brasserie</a>
+											</td>
+											<td>
+												<a href='#'>Commerce Alimentation</a>
+											</td>
+											<td>
+												<a href='#'>Restaurant</a>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href='#'>Boucherie Charcuterie Traiteur</a>
+											</td>
+											<td>
+												<a href='#'>Creperie Pizzeria</a>
+											</td>
+											<td>
+												<a href='#'>Restauration rapide</a>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href='#'>Boulangerie Pâtisserie</a>
+											</td>
+											<td>
+												<a href='#'>Entreprise du bâtiment</a>
+											</td>
+											<td>
+												<a href='#'>Salon de thé</a>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href='#'>Boutique et Magasins</a>
+											</td>
+											<td>
+												<a href='#'>Hôtel Bureau</a>
+											</td>
+											<td>
+												<a href='#'>Tabac Loto Presse</a>
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<a href='#'>Bureaux Locaux Entrepôts</a>
+											</td>
+											<td>
+												<a href='#'>Hôtel Restaurant</a>
+											</td>
+											<td>
+
+											</td>
+										</tr>
+									</table>
+									<ul class='types'>
+										<li>
+											<a href=''>Bar Tabac PMU</a>
+										</li><!--
+										--><li>
+											<a href=''>Bar Brasserie</a>
+										</li><!--
+										--><li>
+											<a href=''>Boucherie Charcuterie Traiteur</a>
+										</li><!--
+										--><li>
+											<a href=''>Boulangerie Pâtisserie</a>
+										</li><!--
+										--><li>
+											<a href=''>Boutique et Magasins</a>
+										</li><!--
+										--><li>
+											<a href=''>Bureaux Locaux Entrepôts</a>
+										</li><!--
+										--><li>
+											<a href=''>Coiffure Esthétique Parfumerie</a>
+										</li><!--
+										--><li>
+											<a href=''>Commerce Alimentation</a>
+										</li><!--
+										--><li>
+											<a href=''>Creperie Pizzeria</a>
+										</li><!--
+										--><li>
+											<a href=''>Entreprise du bâtiment</a>
+										</li><!--
+										--><li>
+											<a href=''>Hôtel Bureau</a>
+										</li><!--
+										--><li>
+											<a href=''>Hôtel Restaurant</a>
+										</li><!--
+										--><li>
+											<a href=''>Murs Commerciaux</a>
+										</li><!--
+										--><li>
+											<a href=''>Restaurant</a>
+										</li><!--
+										--><li>
+											<a href=''>Restauration rapide</a>
+										</li><!--
+										--><li>
+											<a href=''>Salon de thé</a>
+										</li><!--
+										--><li>
+											<a href=''>Tabac Loto Presse</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</li><!--
 						--><li class="cercle-choice">
 							<div class='zone-btn'>
 								<a href='#' class='btn-choice'></a>
@@ -315,151 +460,6 @@
 										</div>
 			                        </div>
 			                    </div>
-								</div>
-							</div>
-						</li><!--
-						--><li>
-							<div class='zone-btn'>
-								<a href='#' class='btn-choice'></a>
-								<div class='bg-choice'></div>
-								<div class='visuel'>
-									<div class='wrapper-alignment'>
-										<div class='content-align-middle'>
-											<img src="img/vente.png" srcset="img/vente.png 1x, img/vente@2x.png 2x" alt="">
-										</div>
-									</div>
-								</div>
-								<div class='zone-txt wrapper-alignment'>
-									<div class='content-align-middle'>
-										<h3>Vendre <br />son affaire</h3>
-										<p>Vente d'un fond de commerce, d'une entreprise, d'un bureau, d'un local&nbsp;commercial…</p>
-									</div>
-								</div>
-							</div><!--
-							--><div class='zone-content'>
-								<div class='content'>
-									<button class='btn-close-choice icon-cross'></button>
-									<table cellpadding='0' cellspacing='0' border='0' class='table-types'>
-										<tr>
-											<td>
-												<a href='#'>Bar Tabac PMU</a>
-											</td>
-											<td>
-												<a href='#'>Coiffure Esthétique Parfumerie</a>
-											</td>
-											<td>
-												<a href='#'>Murs Commerciaux</a>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<a href='#'>Bar Brasserie</a>
-											</td>
-											<td>
-												<a href='#'>Commerce Alimentation</a>
-											</td>
-											<td>
-												<a href='#'>Restaurant</a>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<a href='#'>Boucherie Charcuterie Traiteur</a>
-											</td>
-											<td>
-												<a href='#'>Creperie Pizzeria</a>
-											</td>
-											<td>
-												<a href='#'>Restauration rapide</a>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<a href='#'>Boulangerie Pâtisserie</a>
-											</td>
-											<td>
-												<a href='#'>Entreprise du bâtiment</a>
-											</td>
-											<td>
-												<a href='#'>Salon de thé</a>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<a href='#'>Boutique et Magasins</a>
-											</td>
-											<td>
-												<a href='#'>Hôtel Bureau</a>
-											</td>
-											<td>
-												<a href='#'>Tabac Loto Presse</a>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<a href='#'>Bureaux Locaux Entrepôts</a>
-											</td>
-											<td>
-												<a href='#'>Hôtel Restaurant</a>
-											</td>
-											<td>
-
-											</td>
-										</tr>
-									</table>
-									<ul class='types'>
-										<li>
-											<a href=''>Bar Tabac PMU</a>
-										</li><!--
-										--><li>
-											<a href=''>Bar Brasserie</a>
-										</li><!--
-										--><li>
-											<a href=''>Boucherie Charcuterie Traiteur</a>
-										</li><!--
-										--><li>
-											<a href=''>Boulangerie Pâtisserie</a>
-										</li><!--
-										--><li>
-											<a href=''>Boutique et Magasins</a>
-										</li><!--
-										--><li>
-											<a href=''>Bureaux Locaux Entrepôts</a>
-										</li><!--
-										--><li>
-											<a href=''>Coiffure Esthétique Parfumerie</a>
-										</li><!--
-										--><li>
-											<a href=''>Commerce Alimentation</a>
-										</li><!--
-										--><li>
-											<a href=''>Creperie Pizzeria</a>
-										</li><!--
-										--><li>
-											<a href=''>Entreprise du bâtiment</a>
-										</li><!--
-										--><li>
-											<a href=''>Hôtel Bureau</a>
-										</li><!--
-										--><li>
-											<a href=''>Hôtel Restaurant</a>
-										</li><!--
-										--><li>
-											<a href=''>Murs Commerciaux</a>
-										</li><!--
-										--><li>
-											<a href=''>Restaurant</a>
-										</li><!--
-										--><li>
-											<a href=''>Restauration rapide</a>
-										</li><!--
-										--><li>
-											<a href=''>Salon de thé</a>
-										</li><!--
-										--><li>
-											<a href=''>Tabac Loto Presse</a>
-										</li>
-									</ul>
 								</div>
 							</div>
 						</li>
