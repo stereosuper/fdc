bda7203ec2e7cb99e72b64adea4ad93e4a5b6f38
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-11-08 12:22:37 +0100

    :art: Offres

--- 500_cycle_achat_offres.php
+++ 500_cycle_achat_offres.php
@@ -26,7 +26,63 @@
 
                     <div class='field-content content-log no-header'>
                     <p>En diffusant votre annonce sur le site < activité >, vous faites le bon choix. Pourquoi ? Tout simplement parce que le site < activité > ne s’adresse qu’à des professionnels de votre secteur. Votre annonce aura donc d’autant plus de chance de séduire un futur acquéreur !</p>
+                    <ul class='credits-contact'>
+                        <li>
+                            <a href='#'>Vous souhaitez vendre&nbsp;? <strong>Acheter des crédits</strong> pour déposer votre ou vos annonces</a>
+                        </li><!--
+                        --><li>
+                            <a href='#'>Professionnel de la transaction d’entreprise&nbsp;? <strong>Nous contacter</strong> pour bénéficier de notre offre <i>no limit</i></a>
+                        </li>
+                    </ul>
                     <table class='table-offres-tarifs'>
+                        <tr>
+                            <td class='super-border dark-red'>
+                                <a href='#'>
+                                    <p class='super-huge'>1</p>
+                                    <span class='undercover'>crédit</span>
+                                    <p class='h1'>40&nbsp;€ HT</p>
+                                    <span class="btn-arrow plain big-margin-top">Acheter</span>
+                                </a>
+                            </td>
+                            <td class='super-border red'>
+                                <a href='#'>
+                                    <p class='super-huge'>5</p>
+                                    <span class='undercover'>crédits</span>
+                                    <p class='h1'>100&nbsp;€ HT</p>
+                                    <p>(20€/annonce)</p>
+                                    <span class="btn-arrow plain">Acheter</span>
+                                </a>
+                            </td>
+                            <td class='no-border'>
+                                <p class='h1 credit-annonce'>1 crédit <span class='equal'>=</span> 1 annonce</p>
+                            </td>
+                            <td class='super-border orange'>
+                                <a href='#'>
+                                    <p class='super-huge'>10</p>
+                                    <span class='undercover'>crédits</span>
+                                    <p class='h1'>150&nbsp;€ HT</p>
+                                    <p>(15€/annonce)</p>
+                                    <span class="btn-arrow plain">Acheter</span>
+                                </a>
+                            </td>
+                            <td class='super-border orange-light'>
+                                <a href='#'>
+                                    <p class='super-huge'>20</p>
+                                    <span class='undercover'>crédits</span>
+                                    <p class='h1'>250&nbsp;€ HT</p>
+                                    <p>(10€/annonce)</p>
+                                    <span class="btn-arrow plain">Acheter</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan='5' class='super-border deep-blue option'>
+                                <p class="option-title h1">Option</p>
+                                <p>Vous souhaitez que votre annonce apparaisse systématiquement en tête des résultats de recherche&nbsp;? Choisissez notre offre <span class='notif-pole'>Pole Position</span> à seulement un crédit de plus par annonce.</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- <table class='table-offres-tarifs'>
                         <tr>
                             <td rowspan='2' class='super-border dark-red big-pricing'>
                                 <a href='#'>
@@ -81,7 +137,7 @@
                                 <p>Vous souhaitez que votre annonce apparaisse systématiquement en tête des résultats de recherche&nbsp;? Choisissez notre offre <span class='notif-pole'>Pole Position</span> à seulement un crédit de plus par annonce.</p>
                             </td>
                         </tr>
-                    </table>
+                    </table> -->
                     </div>
                 </div>
             </div>
