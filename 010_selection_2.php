f070d52159adf8fce2f9f23ba79288054c81d907
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-03-29 17:42:34 +0200

    :art: Jobs candidats

---
+++ 010_selection_2.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang='fr-FR' class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang='fr-FR' class='no-js'> <!--<![endif]-->
+
+    <head>
+        <title>Mon compte - Dashboard</title>
+        <?php include_once('includes/head.html'); ?>
+    </head>
+
+    <body class='dashboard'>
+        <?php include_once('includes/header.php'); ?>
+
+        <main id='main' role='main'>
+            <div class='container-small'>
+                <ul class='breadcrumb' vocab='http://schema.org/' typeof='BreadcrumbList'>
+                    <li property='itemListElement' typeof='ListItem'>
+                        <h6><a property='item' typeof='WebPage' href='#'>
+                            <span property='name'>Accueil</span>
+                        </a></h6>
+                    </li><li property='itemListElement' typeof='ListItem'>
+                        <h6><a property='item' typeof='WebPage' href='./'>
+                            <span property='name'>Boulangerie patisserie</span>
+                        </a></h6>
+                    </li>
+                </ul>
+                <h1>150 Candidats</h1>
+                <div class='wrapper-jobs'>
+                    <header class='header-jobs'>
+                        <ul class='filters-team'>
+                            <li>
+                                Poste : <strong>Boulanger / Patissier</strong>
+                            </li>
+                            <li>
+                                Ville : <strong>Nantes (44000)</strong>
+                            </li>
+                            <li>
+                                Mobilité : <strong>Indifférent</strong>
+                            </li>
+                            <li>
+                                Disponibilité de : <strong>1 semaine</strong>
+                            </li>
+                            <li>
+                                <a href='#' class='btn-txt'>Modifier</a>
+                            </li>
+                        </ul>
+                    </header>
+                    <div class='content-jobs'>
+                        <ul class='list-cards'>
+                            <li>
+                                <header>
+                                    <div class='img-applicant' style='background: url("../img/applicant.jpg");'></div>
+                                    <a class='btn-applicant btn-cv' href='#'><span class='icon-download'></span>Télécharger le CV</a>
+                                </header>
+                                <div class='content-card'>
+                                    <div class='wrapper-name-distance'>
+                                        <h2 class='name'>
+                                            Pierre T.
+                                        </h2>
+                                        <span class='distance'>
+                                            <span class='icon-pin'></span>1 km
+                                        </span>
+                                    </div>
+                                    <a class='btn-applicant btn-memo' href='#'><span class='icon-pin2'></span>1 mémo(s)</a>
+                                    <h3 class='applicant-job'>
+                                        Chef Boulanger - Patissier
+                                    </h3>
+                                    <p class='applicant-desc'>5 ans d’expérience aux cotés de Vincent Guerlais</p>
+                                </div>
+                                <footer>
+                                    <div class='wrapper-tel-applicant'>
+                                        <div class='tel-applicant'>Tel : <span class='tel-number'>06 94 32 16 83</span></div>
+                                        <a class='btn-applicant btn-show-infos' href='#'>Afficher</a>
+                                    </div>
+                                    <div class='wrapper-mail-applicant'>
+                                        <a class='mail-applicant' href='#'>pierre.tromelin2@gmail.com</a>
+                                    </div>
+                                    <div class='wrapper-btn-applicant'>
+                                        <a class='btn-invalid-applicant icon-cross-big' href='#'></a>
+                                        <a class='btn-valid-applicant icon-checkmark-big' href='#'></a>
+                                    </div>
+                                </footer>
+                            </li>
+                            <li>
+                                <header>
+                                    <div class='img-applicant' style='background: url("../img/applicant.jpg");'></div>
+                                    <a class='btn-applicant btn-cv' href='#'><span class='icon-download'></span>Télécharger le CV</a>
+                                </header>
+                                <div class='content-card'>
+                                    <div class='wrapper-name-distance'>
+                                        <h2 class='name'>
+                                            Pierre T.
+                                        </h2>
+                                        <span class='distance'>
+                                            <span class='icon-pin'></span>1 km
+                                        </span>
+                                    </div>
+                                    <a class='btn-applicant btn-memo' href='#'><span class='icon-pin2'></span>1 mémo(s)</a>
+                                    <h3 class='applicant-job'>
+                                        Chef Boulanger - Patissier
+                                    </h3>
+                                    <p class='applicant-desc'>5 ans d’expérience aux cotés de Vincent Guerlais</p>
+                                </div>
+                                <footer>
+                                    <div class='wrapper-tel-applicant'>
+                                        <div class='tel-applicant'>Tel : <span class='tel-number'>06 94 32 16 83</span></div>
+                                        <a class='btn-applicant btn-show-infos' href='#'>Afficher</a>
+                                    </div>
+                                    <div class='wrapper-mail-applicant'>
+                                        <a class='mail-applicant' href='#'>pierre.tromelin2@gmail.com</a>
+                                    </div>
+                                    <div class='wrapper-btn-applicant'>
+                                        <a class='btn-invalid-applicant icon-cross-big' href='#'></a>
+                                        <a class='btn-valid-applicant icon-checkmark-big' href='#'></a>
+                                    </div>
+                                </footer>
+                            </li>
+                            <li>
+                                <header>
+                                    <div class='img-applicant' style='background: url("../img/applicant.jpg");'></div>
+                                    <a class='btn-applicant btn-cv' href='#'><span class='icon-download'></span>Télécharger le CV</a>
+                                </header>
+                                <div class='content-card'>
+                                    <div class='wrapper-name-distance'>
+                                        <h2 class='name'>
+                                            Pierre T.
+                                        </h2>
+                                        <span class='distance'>
+                                            <span class='icon-pin'></span>1 km
+                                        </span>
+                                    </div>
+                                    <a class='btn-applicant btn-memo' href='#'><span class='icon-pin2'></span>1 mémo(s)</a>
+                                    <h3 class='applicant-job'>
+                                        Chef Boulanger - Patissier
+                                    </h3>
+                                    <p class='applicant-desc'>5 ans d’expérience aux cotés de Vincent Guerlais</p>
+                                </div>
+                                <footer>
+                                    <div class='wrapper-tel-applicant'>
+                                        <div class='tel-applicant'>Tel : <span class='tel-number'>06 94 32 16 83</span></div>
+                                        <a class='btn-applicant btn-show-infos' href='#'>Afficher</a>
+                                    </div>
+                                    <div class='wrapper-mail-applicant'>
+                                        <a class='mail-applicant' href='#'>pierre.tromelin2@gmail.com</a>
+                                    </div>
+                                    <div class='wrapper-btn-applicant'>
+                                        <a class='btn-invalid-applicant icon-cross-big' href='#'></a>
+                                        <a class='btn-valid-applicant icon-checkmark-big' href='#'></a>
+                                    </div>
+                                </footer>
+                            </li>
+                            <li>
+                                <header>
+                                    <div class='img-applicant' style='background: url("../img/applicant.jpg");'></div>
+                                    <a class='btn-applicant btn-cv' href='#'><span class='icon-download'></span>Télécharger le CV</a>
+                                </header>
+                                <div class='content-card'>
+                                    <div class='wrapper-name-distance'>
+                                        <h2 class='name'>
+                                            Pierre T.
+                                        </h2>
+                                        <span class='distance'>
+                                            <span class='icon-pin'></span>1 km
+                                        </span>
+                                    </div>
+                                    <a class='btn-applicant btn-memo' href='#'><span class='icon-pin2'></span>1 mémo(s)</a>
+                                    <h3 class='applicant-job'>
+                                        Chef Boulanger - Patissier
+                                    </h3>
+                                    <p class='applicant-desc'>5 ans d’expérience aux cotés de Vincent Guerlais</p>
+                                </div>
+                                <footer>
+                                    <div class='wrapper-tel-applicant'>
+                                        <div class='tel-applicant'>Tel : <span class='tel-number'>06 94 32 16 83</span></div>
+                                        <a class='btn-applicant btn-show-infos' href='#'>Afficher</a>
+                                    </div>
+                                    <div class='wrapper-mail-applicant'>
+                                        <a class='mail-applicant' href='#'>pierre.tromelin2@gmail.com</a>
+                                    </div>
+                                    <div class='wrapper-btn-applicant'>
+                                        <a class='btn-invalid-applicant icon-cross-big' href='#'></a>
+                                        <a class='btn-valid-applicant icon-checkmark-big' href='#'></a>
+                                    </div>
+                                </footer>
+                            </li>
+                        </ul>
+                        <div class='wrapper-map-jobs'>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php include_once('includes/footer.html'); ?>
+
+    </body>
+
+</html>
