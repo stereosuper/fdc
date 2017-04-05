df086f41dbbd1fb977b770ac1ba34e7adecc3012
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-04-05 09:18:25 +0200

    :art: Search results with autocomplete

--- 010_selection_2.php
+++ 010_selection_2.php
@@ -4,7 +4,7 @@
 
     <head>
         <title></title>
-        <?php include_once('includes/head.html'); ?>
+        <?php include_once('includes/head_autocomplete.html'); ?>
     </head>
 
     <body class='dashboard'>
